e122971a4b8b7bf10b356c847c2ac677a9adf16e
autobolt <autobots@9thbit.net>
2026-05-20 12:35:11 -0600

    Load auth sanitizer from resolved gemspec

--- lib/omniauth/ldap/adaptor.rb
+++ lib/omniauth/ldap/adaptor.rb
@@ -11,13 +11,12 @@ require "sasl"
 module OmniAuth
   module LDAP
     AUTH_SANITIZER = unless const_defined?(:AUTH_SANITIZER, false)
-      auth_sanitizer_loader_path = $LOAD_PATH
-        .map { |load_path| File.join(load_path, "auth_sanitizer/loader.rb") }
-        .find { |path| File.file?(path) }
-
-      unless auth_sanitizer_loader_path
-        raise LoadError, "cannot load such file -- auth_sanitizer/loader"
-      end
+      auth_sanitizer_spec = Gem.loaded_specs["auth-sanitizer"] ||
+        Gem::Specification.find_by_name("auth-sanitizer")
+      auth_sanitizer_loader_path = File.join(
+        auth_sanitizer_spec.full_gem_path,
+        "lib/auth_sanitizer/loader.rb",
+      )
 
       auth_sanitizer_loader_namespace = Module.new
       auth_sanitizer_loader_namespace.module_eval(
